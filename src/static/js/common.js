a2a6f27a48b804bc5bd17e41ce98cdde9da9d95f
Aslan <berezg86@gmail.com>
2020-09-07 13:04:10 +0300

    Баннер в шапке, мобильное меню

--- src/static/js/common.js
+++ src/static/js/common.js
@@ -1,3 +1,8 @@
 $(document).ready(function () {
+    $(".burger").click(function () {
+        $(this).toggleClass('is-active');
+        $('.dropdown-menu').toggleClass('is-active');
+    });
+
     $('.select select').selectric();
 });
